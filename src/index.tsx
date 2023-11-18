bda08d0581964ee0dc4d8321b1660f40b2b8f225
mujdecisy <mujdecisy@gmail.com>
2023-11-19 02:09:38 +0300

    feat: add calendar and hour stepper to main

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,19 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import ReduxProvider from "./redux/provider";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
     <ReduxProvider>
       <App />
     </ReduxProvider>
+    </LocalizationProvider>
   </React.StrictMode>
 );
 
--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,21 +1,78 @@
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { updatePageState } from '../redux/slicePage';
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { updatePageState } from "../redux/slicePage";
+import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
+import { DayCalendarSkeleton } from "@mui/x-date-pickers/DayCalendarSkeleton";
+import { Card, Step, StepLabel, Stepper, SvgIconTypeMap } from "@mui/material";
+import { CheckBoxOutlineBlank } from "@mui/icons-material";
+import { OverridableComponent } from "@mui/material/OverridableComponent";
+
+interface Step {
+    label: string
+    icon: OverridableComponent<SvgIconTypeMap<{}, "svg">>
+}
 
 export default function Main() {
-    const dispatch = useDispatch();
-    useEffect(()=>{
-        dispatch(updatePageState({
-            navItems: [
-                {icon: "Info", link: "./about"}
-            ],
-            title: ""
-        }));
-    }, [dispatch]);
+  const dispatch = useDispatch();
+  const [steps, setSteps] = useState([] as Step[]);
+  const [activeStep, setActiveStep] = useState(0);
 
-    return (
-        <div>
-            <h1>HELLO</h1>
-        </div>
-    )
+  useEffect(()=>{
+    const localSteps = [] as Step[];
+    const now = new Date();
+    let localActiveStep = 0;
+    for (let i=0; i<23; i++) {
+        if (now.getHours() == i) {
+            localActiveStep = (now.getMinutes() < 30)? i*2 : i*2+1
+        }
+        localSteps.push({
+            label: `${String(i).padStart(2,'0')}:00`,
+            icon: CheckBoxOutlineBlank
+        });
+        localSteps.push({
+            label: `${String(i).padStart(2,'0')}:30`,
+            icon: CheckBoxOutlineBlank
+        });
+    }
+    setSteps(localSteps);
+    setActiveStep(localActiveStep);
+  }, [])
+
+  useEffect(() => {
+    dispatch(
+      updatePageState({
+        navItems: [{ icon: "Info", link: "./about" }],
+        title: "",
+      })
+    );
+  }, [dispatch]);
+
+  return (
+    <div>
+      <Card variant="outlined">
+        <DateCalendar
+          defaultValue={new Date()}
+          loading={false}
+          onMonthChange={() => {
+            console.log("month changed");
+          }}
+          renderLoading={() => <DayCalendarSkeleton />}
+        />
+      </Card>
+
+        <Stepper
+          alternativeLabel
+          activeStep={activeStep}
+          className="mt-10 overflow-y-auto py-5"
+        >
+          {steps.map((e) => (
+            <Step key={e.label}>
+              <StepLabel StepIconComponent={e.icon}>
+                {e.label}
+              </StepLabel>
+            </Step>
+          ))}
+        </Stepper>
+    </div>
+  );
 }
